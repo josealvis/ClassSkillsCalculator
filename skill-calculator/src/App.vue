e8da072785078a47bc3dcf8b8d81fef00026f1f1
jose alvis <jalvis1491@gmail.com>
2018-06-30 05:03:53 +0300

    Add Questionary component

--- skill-calculator/src/App.vue
+++ skill-calculator/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Questionary students = "45"/>
+   <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Questionary from './components/Questionary.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Questionary
   }
 }
 </script>
---
+++ skill-calculator/src/components/Questionary.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <h2> Estudiantes: {{students}}</h2>
+     <div>{{count}}</div>
+     <button v-on:click="click()">
+  Enlarge text
+</button>
+</div>
+</template>
+
+<script>
+
+
+let count = 0;
+
+function click(){
+    this.count++;
+}
+
+
+function data(){
+return {count} 
+}
+
+
+
+export default {
+  name: 'Questionary',
+  props: {
+    students: Number
+  },
+  data,
+  methods: {
+       click
+
+  }
+}
+</script>
